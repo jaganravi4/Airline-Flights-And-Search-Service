375e798784ad8b0dca54b646301d610e2890b9bb
jaganravi4 <jaganravichandran1@gmail.com>
2026-06-19 14:45:30 +0530

    Added filters to get all cities

--- src/repositories/city-repository.js
+++ src/repositories/city-repository.js
@@ -1,3 +1,4 @@
+const { Op } = require("sequelize");
 const { City } = require("../models/index");
 
 // TODO: Implement Singleton Pattern
@@ -48,8 +49,18 @@ class CityRepository {
         }
     }
 
-    async getAllCities() {
+    async getAllCities(filter) {
         try {
+            if (filter.name) {
+                const cities = await City.findAll({
+                    where: {
+                        name: {
+                            [Op.startsWith]: filter.name,
+                        },
+                    },
+                });
+                return cities;
+            }
             const cities = await City.findAll();
             return cities;
         } catch (error) {
